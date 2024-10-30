9770b9d85e179bcc44aa1b83bac3cbf6f314567c
Даша Побережник <dasosha2017@gmail.com>
2024-10-30 17:09:00 +0200

    +

--- Hw_City.java
+++ Hw_City.java
@@ -1,28 +1,53 @@
-public class Hw_City {
-    private final String name;
-    private final int population;
+import java.util.Scanner;
 
-    public Hw_City(String name, int population) {
+public class Hw_City {
+    private String name;
+    private String region;
+    private String country;
+    private int population;
+    private String postalCode;
+    private String phoneCode;
+
+
+    public Hw_City(String name, String region, String country, int population, String postalCode, String phoneCode) {
         this.name = name;
+        this.region = region;
+        this.country = country;
         this.population = population;
+        this.postalCode = postalCode;
+        this.phoneCode = phoneCode;
     }
 
-    public Hw_City(String name) {
-        this(name, 0);
+
+    public void inputData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите название города: ");
+        name = scanner.nextLine();
+        System.out.print("Введите название региона: ");
+        region = scanner.nextLine();
+        System.out.print("Введите название страны: ");
+        country = scanner.nextLine();
+        System.out.print("Введите количество жителей: ");
+        population = scanner.nextInt();
+        scanner.nextLine(); // Очистка буфера
+        System.out.print("Введите почтовый индекс: ");
+        postalCode = scanner.nextLine();
+        System.out.print("Введите телефонный код: ");
+        phoneCode = scanner.nextLine();
     }
 
     public void displayInfo() {
-        System.out.println("Город: " + name + ", Население: " + population);
+        System.out.println("Город: " + name);
+        System.out.println("Регион: " + region);
+        System.out.println("Страна: " + country);
+        System.out.println("Население: " + population);
+        System.out.println("Почтовый индекс: " + postalCode);
+        System.out.println("Телефонный код: " + phoneCode);
     }
 
     public static void main(String[] args) {
-        Hw_City city1 = new Hw_City("Винница", 370000);
-        city1.displayInfo();
-
-        Hw_City city2 = new Hw_City("Киев");
-        city2.displayInfo();
-
-        city1.displayInfo();
-        city2.displayInfo();
+        Hw_City city = new Hw_City("", "", "", 0, "", "");
+        city.inputData();
+        city.displayInfo();
     }
 }
--- Hw_Person.java
+++ Hw_Person.java
@@ -1,28 +1,67 @@
+import java.util.Scanner;
+
 public class Hw_Person {
-    private final String name;
-    private final int age;
+    private String fullName; // ФИО
+    private String birthDate; // Дата рождения
+    private String phone; // Контактный телефон
+    private String city; // Город
+    private String country; // Страна
+    private String homeAddress; // Домашний адрес
 
-    public Hw_Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    // Конструктор
+    public Hw_Person(String fullName, String birthDate, String phone, String city, String country, String homeAddress) {
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.city = city;
+        this.country = country;
+        this.homeAddress = homeAddress;
     }
 
-    public String getName() {
-        return name;
+    // Метод для ввода данных
+    public void inputData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ФИО: ");
+        fullName = scanner.nextLine();
+        System.out.print("Введите дату рождения: ");
+        birthDate = scanner.nextLine();
+        System.out.print("Введите контактный телефон: ");
+        phone = scanner.nextLine();
+        System.out.print("Введите город: ");
+        city = scanner.nextLine();
+        System.out.print("Введите страну: ");
+        country = scanner.nextLine();
+        System.out.print("Введите домашний адрес: ");
+        homeAddress = scanner.nextLine();
     }
 
-    public void setName(String name) {
-
+    // Метод для вывода данных
+    public void displayInfo() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Дата рождения: " + birthDate);
+        System.out.println("Контактный телефон: " + phone);
+        System.out.println("Город: " + city);
+        System.out.println("Страна: " + country);
+        System.out.println("Домашний адрес: " + homeAddress);
     }
 
-    public int getAge() {
-        return age;
+    // Методы доступа
+    public String getFullName() {
+        return fullName;
     }
 
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    // Метод main для тестирования класса
     public static void main(String[] args) {
-        Hw_Person person1 = new Hw_Person("Иван", 30);
-        System.out.println("Имя: " + person1.getName());
-        System.out.println("Возраст: " + person1.getAge());
+        Hw_Person person = new Hw_Person("", "", "", "", "", "");
+        person.inputData();
+        person.displayInfo();
 
+        // Примеры использования методов getFullName() и getBirthDate()
+        System.out.println("Полученное ФИО: " + person.getFullName());
+        System.out.println("Полученная дата рождения: " + person.getBirthDate());
     }
 }
