fedff03f02f7a7f692b7ea7b9300a38d5bff600f
Даша Побережник <dasosha2017@gmail.com>
2024-10-30 19:15:01 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Hw_Person.java

--- LibraryApp.java
+++ LibraryApp.java
@@ -1,21 +1,10 @@
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-// Newspaper.java - класс для газеты
-import java.util.Date;
-import java.util.List;
 
-// Almanac.java - класс для альманаха
-import java.util.List;
 
-// LibraryCatalog.java - класс для каталога библиотеки
-import java.util.ArrayList;
-import java.util.List;
 
-// LibraryApp.java - основной класс для запуска приложения
 import java.util.Arrays;
-import java.util.Date;
+import java.util.List;
 
 public class LibraryApp {
     public static void main(String[] args) {
@@ -23,7 +12,7 @@ public class LibraryApp {
 
         Book book1 = new Book("Название Книги", "Автор Книги", "Жанр Книги", 300);
         Newspaper newspaper1 = new Newspaper("Название Газеты", new Date(), Arrays.asList("Заголовок 1", "Заголовок 2"));
-        Almanac almanac1 = new Almanac("Название Альманаха", Arrays.asList(book1));
+        Almanac almanac1 = new Almanac("Название Альманаха", List.of(book1));
 
         catalog.addItem(book1);
         catalog.addItem(newspaper1);
